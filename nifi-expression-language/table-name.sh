9b62a8d2011fe432627441b17990deca582ef367
Luke <lsh-0@users.noreply.github.com>
2019-06-13 11:52:16 +0930

    table-name.sh, examples are not elaborated on if input provided

--- nifi-expression-language/table-name.sh
+++ nifi-expression-language/table-name.sh
@@ -12,19 +12,19 @@ function table_name {
         '${filename:substring(0, ${filename:length():minus(21)}):replaceFirst("ejp_query_tool_query_id_", ""):toLower():replace(":", ""):replace("_-_", "_")}'
 }
 
-echo "old-style EJP filenames"
-
-table_name "ejp_query_tool_query_id_380_DataScience:_Early_Career_Researchers_2018_09_03_eLife.csv"
-table_name "ejp_query_tool_query_id_489_DataScience:_Editor_Keywords_2018_09_03_eLife.csv"
-table_name "ejp_query_tool_query_id_455_DataScience:_Editors_2018_09_03_eLife.csv"
-
-echo
-echo "new-style EJP filenames"
-
-table_name "ejp_query_tool_query_id_DataScience:_Early_Career_Researchers_2018_09_03_eLife.csv"
-table_name "ejp_query_tool_query_id_DataScience:_Editor_Keywords_2018_09_03_eLife.csv"
-table_name "ejp_query_tool_query_id_DataScience:_Editors_2018_09_03_eLife.csv"
-
 if [ -n "$1" ]; then
     table_name "$1"
+else
+    echo "old-style EJP filenames"
+
+    table_name "ejp_query_tool_query_id_380_DataScience:_Early_Career_Researchers_2018_09_03_eLife.csv"
+    table_name "ejp_query_tool_query_id_489_DataScience:_Editor_Keywords_2018_09_03_eLife.csv"
+    table_name "ejp_query_tool_query_id_455_DataScience:_Editors_2018_09_03_eLife.csv"
+
+    echo
+    echo "new-style EJP filenames"
+
+    table_name "ejp_query_tool_query_id_DataScience:_Early_Career_Researchers_2018_09_03_eLife.csv"
+    table_name "ejp_query_tool_query_id_DataScience:_Editor_Keywords_2018_09_03_eLife.csv"
+    table_name "ejp_query_tool_query_id_DataScience:_Editors_2018_09_03_eLife.csv"
 fi
