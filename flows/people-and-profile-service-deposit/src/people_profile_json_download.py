c21509789eff30e916eeda19fcdd5d5fefea5afb
tayowonibi <tayowonibi@yahoo.com>
2019-06-14 13:45:45 +0100

    code change based on pull request comments

--- flows/people-and-profile-service-deposit/src/people_profile_json_download.py
+++ flows/people-and-profile-service-deposit/src/people_profile_json_download.py
@@ -5,17 +5,11 @@ Writes the data primarily to stdout
 @author: mowonibi
 """
 
-import urllib.request, json, logging, sys, datetime
+from urllib import request, parse, error
+import json
+import sys
+import datetime
 from datetime import timezone
-from typing import Dict
-
-# Create a custom logger
-logger = logging.getLogger(__name__)
-c_handler = logging.StreamHandler()
-c_handler.setLevel(logging.WARNING)
-c_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-c_handler.setFormatter(c_format)
-logger.addHandler(c_handler)
 
 
 def now():
@@ -29,59 +23,54 @@ def download_data(data_url: str):
     if not possible return empty dict
     """
     try:
-        response = urllib.request.urlopen(data_url)
+        response = request.urlopen(data_url)
         downloaded_data = response.read()
-        downloaded_data = downloaded_data.decode('utf-8')
+        downloaded_data = downloaded_data.decode("utf-8")
         try:
-            downloaded_data = json.loads(downloaded_data, encoding='utf-8')
+            downloaded_data = json.loads(downloaded_data, encoding="utf-8")
         except json.JSONDecodeError:
-            logger.warning('data not decoded to json')
-    except urllib.error.URLError:
-        logger.warning('data not downloaded')
+            sys.stderr.write("data in %s not decoded to json" % data_url)
+    except error.URLError:
+        sys.stderr.write("data in %s not downloaded" % data_url)
         downloaded_data = dict()
 
     return downloaded_data
 
 
-def compose_url(api_base_url, url_param_dict: Dict[str, str]):
+def download_page_by_page(api_base_url, time_now, output=None, page_size=50):
     """
-    compose url from base api url and parameter dictionary
-
+    Download all data page by page from api_base_url
     """
-    url_param = ''
-    url_separator = '' if api_base_url.endswith('?') else '?'
-    for key, value in url_param_dict.items():
-        url_param += str(key) + '=' + str(value) +'&'
-    return api_base_url + url_separator + url_param
-
-
-def download_page_by_page(api_base_url, output=None, page_size=50):
     current_page = 1
     get_more_data = True
     out = output or print
-    time_now = now()
 
     while get_more_data:
-        data_page_url = compose_url(api_base_url, url_param_dict={'per-page': page_size, 'page': current_page})
+        # create url
+        url_separator = "" if api_base_url.endswith("?") else "?"
+        url_param_dict = {"per-page": page_size, "page": current_page}
+        params = "&".join(
+            ["%s=%s" % (k, parse.quote(str(v))) for k, v in url_param_dict.items()]
+        )
+        data_page_url = api_base_url + url_separator + params
+        # download data
         json_data = download_data(data_page_url)
-        get_more_data = json_data.get('total', 1) > page_size * current_page
+        get_more_data = json_data.get("total", 0) > page_size * current_page
 
-        for row in json_data.get('items', []):
+        for row in json_data.get("items", []):
             row["imported_timestamp"] = time_now
             out(json.dumps(row))
 
         current_page += 1
 
 
-if __name__ == '__main__' :
-    args = sys.argv[1:]
-    root_profile_url = args[0]
-
+if __name__ == "__main__":
+    ARGS = sys.argv[1:]
+    ROOT_PROFILE_URL = ARGS[0]
     try:
-        page_size = int(args[1])
-        page_size = 100 if page_size > 100 else page_size
+        PAGE_SIZE = int(ARGS[1])
+        PAGE_SIZE = 100 if PAGE_SIZE > 100 else PAGE_SIZE
     except IndexError:
-        page_size = 100
+        PAGE_SIZE = 100
 
-    download_page_by_page(root_profile_url, page_size=page_size)
-
+    download_page_by_page(ROOT_PROFILE_URL, time_now=now(), page_size=PAGE_SIZE)
--- flows/people-and-profile-service-deposit/test.py
+++ flows/people-and-profile-service-deposit/test.py
@@ -1,52 +1,45 @@
 import unittest
 import json
-from unittest.mock import  patch
+from unittest.mock import patch
 
 from src import people_profile_json_download
-
-
+from unittest.mock import MagicMock
 
 
 class TestPeopleProfileJsonDownload(unittest.TestCase):
-
     def test_download_page_by_page(self):
-        output_list=list()
+        output_list = list()
+        time_now = people_profile_json_download.now()
+        response = b'{"total": 2, "items": [{"id": "testid1", "name": {"preferred": "name1 pref", "index": "name1, index"}, "orcid": "0005-0007-1754-3384"}, {"id": "id2", "name": {"preferred": "pref name2", "index": "name, index2"}, "orcid": "0340-0343-4760-8257"}]}'
 
         def append_to_list(json_row):
             json_row = json.loads(json_row)
             output_list.append(json_row)
 
-        response = b"{\"total\": 2, \"items\": [{\"id\": \"testid1\", \"name\": {\"preferred\": \"name1 pref\", \"index\": \"name1 ,index\"}, \"orcid\": \"0005-0007-1754-3384\"}, {\"id\": \"id2\", \"name\": {\"preferred\": \"pref name2\", \"index\": \"name, index2\"}, \"orcid\": \"0340-0343-4760-8257\"}]}"
-        with patch('urllib.request.urlopen', return_value=FakeResponse(data=response)) as mock_request:
-            api_base_url_list = ['https://testurl.url']
+        with patch("urllib.request.urlopen") as mock_urlopen:
+            response_mock = MagicMock()
+            response_mock.read.return_value = response
+            mock_urlopen.return_value = response_mock
+            api_base_url_list = ["https://testurl.url"]
             for api_base_url in api_base_url_list:
                 output_list.clear()
-                people_profile_json_download.download_page_by_page(api_base_url, output=append_to_list)
-
+                people_profile_json_download.download_page_by_page(
+                    api_base_url, time_now=time_now, output=append_to_list
+                )
                 self.assertTrue(len(output_list) == 2)
-                print(len(output_list), output_list[0], type(output_list[1]))
-                self.assertTrue('imported_timestamp' in output_list[0])
-                self.assertTrue('id' in output_list[0])
-                self.assertTrue('name' in output_list[0])
+                self.assertTrue("imported_timestamp" in output_list[0])
+                self.assertTrue("id" in output_list[0])
+                self.assertTrue("name" in output_list[0])
+                self.assertTrue(output_list[0].get("id") == "testid1")
+                self.assertTrue(output_list[0].get("imported_timestamp") == time_now)
+                self.assertTrue(
+                    output_list[0].get("name").get("preferred") == "name1 pref"
+                )
+                self.assertTrue(
+                    output_list[0].get("name").get("index") == "name1, index"
+                )
+                self.assertTrue(output_list[0].get("orcid") == "0005-0007-1754-3384")
 
 
-class FakeResponse:
-    status: int
-    data: bytes
-
-    def __init__(self, *, data: bytes):
-        self.data = data
-
-    def read(self):
-        self.status = 200 if self.data is not None else 404
-        return self.data
-
-    def close(self):
-        pass
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
-
-
-
