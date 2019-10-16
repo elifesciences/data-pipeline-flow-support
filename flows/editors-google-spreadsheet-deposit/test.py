f011fc8214b3fc74982fc8cb35a76559f597276c
michael <m.owonibi@elifesciences.org>
2019-10-16 12:34:56 +0100

    #5152 lint the files change based on pr

--- flows/editors-google-spreadsheet-deposit/test.py
+++ flows/editors-google-spreadsheet-deposit/test.py
@@ -1,3 +1,6 @@
+"""
+unittest editorscsv2json.py
+"""
 import unittest
 from io import StringIO
 import json
@@ -6,20 +9,47 @@ from src import editorscsv2json
 
 class TestEditorsCsv2Json(unittest.TestCase):
     def test_process_file_stream_content(self):
-
         self.maxDiff = None
         test_text = (
-            "Name,Subject areas,Keywords,Research interests,Website URL,PubMed URL,Relevant PubMed URLs\n"
-            '"name1","subject1, subject4, subject5","keyword1, keyword2, keyword3","body of research, interest text","http://wwww.eexample.com:8080/sds?ruiu=researcher","http://pubmedurl.org/something?arg=value","url1, url2"\n'
-            '"name1","subject1","keyword1, keyword2, keyword3","body of research, interest text","http://wwww.eexample.com:8080/sds?ruiu=researcher","http://pubmedurl.org/something?arg=value","url3, url4"\n'
-            '"name2","subject7","keyword1, keyword4, keyword5","body of research interest text2","http://wwww.eexample2.com:8080/sds?ruiu=researcher","http://pubmedurl.org/something2?arg=value","url5, url6"\n'
+            "Name,Subject areas,Keywords,Research interests,Website URL,"
+            "PubMed URL,Relevant PubMed URLs\n"
+            '"name1","subject1, subject4, subject5","keyword1, keyword2, keyword3",'
+            '"body of research, interest text",'
+            '"http://wwww.eexample.com:8080/sds?ruiu=researcher",'
+            '"http://pubmedurl.org/something?arg=value","url1, url2"\n'
+            '"name1","subject1","keyword1, keyword2, keyword3",'
+            '"body of research, interest text",'
+            '"http://wwww.eexample.com:8080/sds?ruiu=researcher",'
+            '"http://pubmedurl.org/something?arg=value","url3, url4"\n'
+            '"name2","subject7","keyword1, keyword4, keyword5",'
+            '"body of research interest text2",'
+            '"http://wwww.eexample2.com:8080/sds?ruiu=researcher",'
+            '"http://pubmedurl.org/something2?arg=value","url5, url6"\n'
         )
         current_time = "2019-10-16 08:24:32 +0100"
         expected_values = list()
 
-        row_1 = '{"name": "name1","subject_areas":"subject1, subject4, subject5", "keywords": "keyword1, keyword2, keyword3", "research_interests": "body of research, interest text", "website_url": "http:\/\/wwww.eexample.com:8080\/sds?ruiu=researcher", "pubmed_url": "http:\/\/pubmedurl.org\/something?arg=value","relevant_pubmed_urls":"url1, url2", "editor_role": "BRE", "imported_timestamp":"2019-10-16T07:24:32Z"}'
-        row_2 = '{"name": "name1", "subject_areas":"subject1", "keywords": "keyword1, keyword2, keyword3", "research_interests": "body of research, interest text", "website_url": "http:\/\/wwww.eexample.com:8080\/sds?ruiu=researcher", "pubmed_url": "http:\/\/pubmedurl.org\/something?arg=value","relevant_pubmed_urls":"url3, url4","editor_role": "BRE", "imported_timestamp":"2019-10-16T07:24:32Z"}'
-        row_3 = '{"name": "name2","subject_areas":"subject7", "keywords": "keyword1, keyword4, keyword5", "research_interests": "body of research interest text2", "website_url": "http:\/\/wwww.eexample2.com:8080\/sds?ruiu=researcher", "pubmed_url": "http:\/\/pubmedurl.org\/something2?arg=value","relevant_pubmed_urls":"url5, url6","editor_role": "BRE", "imported_timestamp":"2019-10-16T07:24:32Z"}'
+        row_1 = r'{"name": "name1","subject_areas":"subject1, subject4, subject5",' \
+                r' "keywords": "keyword1, keyword2, keyword3", "research_interests": ' \
+                r'"body of research, interest text",' \
+                r' "website_url": "http://wwww.eexample.com:8080/sds?ruiu=researcher", ' \
+                r'"pubmed_url": "http://pubmedurl.org/something?arg=value",' \
+                r'"relevant_pubmed_urls":"url1, url2",' \
+                r' "editor_role": "BRE", "imported_timestamp":"2019-10-16T07:24:32Z"}'
+        row_2 = r'{"name": "name1", "subject_areas":"subject1", ' \
+                r'"keywords": "keyword1, keyword2, keyword3", ' \
+                r'"research_interests": "body of research, interest text",' \
+                r' "website_url": "http://wwww.eexample.com:8080/sds?ruiu=researcher", ' \
+                r'"pubmed_url": "http://pubmedurl.org/something?arg=value",' \
+                r'"relevant_pubmed_urls":"url3, url4",' \
+                r'"editor_role": "BRE", "imported_timestamp":"2019-10-16T07:24:32Z"}'
+        row_3 = r'{"name": "name2","subject_areas":"subject7", ' \
+                r'"keywords": "keyword1, keyword4, keyword5", ' \
+                r'"research_interests": "body of research interest text2", ' \
+                r'"website_url": "http://wwww.eexample2.com:8080/sds?ruiu=researcher", ' \
+                r'"pubmed_url": "http://pubmedurl.org/something2?arg=value",' \
+                r'"relevant_pubmed_urls":"url5, url6",' \
+                r'"editor_role": "BRE", "imported_timestamp":"2019-10-16T07:24:32Z"}'
         expected_values.append(json.loads(row_1))
         expected_values.append(json.loads(row_2))
         expected_values.append(json.loads(row_3))
@@ -37,7 +67,7 @@ class TestEditorsCsv2Json(unittest.TestCase):
             time_now=current_time,
         )
 
-        for json_object_index in range(len(output_list)):
+        for json_object_index, _ in enumerate(output_list):
             self.assertEqual(
                 output_list[json_object_index],
                 expected_values[json_object_index],
