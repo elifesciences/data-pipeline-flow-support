d645f38ab3d8e3a2227cd3421a9821df87e231a5
michael <m.owonibi@elifesciences.org>
2019-10-16 10:49:37 +0100

    #5152 import public editor profile based on new schema use same timestamp for all sheets in the spreadsheet imported

--- flows/editors-google-spreadsheet-deposit/src/editorscsv2json.py
+++ flows/editors-google-spreadsheet-deposit/src/editorscsv2json.py
@@ -4,21 +4,25 @@ it must not have any dependencies other than Python3"""
 import sys, json, csv, fileinput, datetime
 from datetime import timezone
 
-
 def normalise(colheader):
     return colheader.lower().strip().replace(" ", "_").replace(")", "").replace("(", "")
 
 
-def now():
-    dtobj = datetime.datetime.now(timezone.utc)
+def get_utc_time(string_datetime:str):
+    dtobj = datetime.datetime.strptime(string_datetime, "%Y-%m-%d %H:%M:%S %z")
+    dtobj = dtobj.astimezone(tz=timezone.utc)
     return dtobj.strftime("%Y-%m-%dT%H:%M:%SZ")
 
+def now():
+    dtobj = datetime.datetime.now(timezone.utc)
+    return dtobj.strftime("%Y-%m-%d %H:%M:%S %z")
 
-def first(lst):
+
+def get_args(lst):
     try:
-        return lst[0]
+        return lst[0], lst[1]
     except IndexError:
-        return None
+        return None, now()
 
 
 def empty_fields_are_null(row):
@@ -29,17 +33,15 @@ def empty_fields_are_null(row):
     return row
 
 
-def main(input=None, output=None, filename=None):
+def main(input=None, output=None, filename=None, time_now=None):
     # fileinput.input reads sys.argv for input if we don't specify what it should be reading
     stdin = ["-"]
     fh = input or fileinput.input(stdin)
     out = output or print
-    time_now = now()
 
     header_reader = csv.reader(fh)
     header = list(map(normalise, next(header_reader)))
     editor_role = " ".join(filename.split('_')[1:]) if filename else ""
-
     reader = csv.DictReader(fh, fieldnames=header)
 
     for row in reader:
@@ -52,4 +54,5 @@ def main(input=None, output=None, filename=None):
 
 if __name__ == "__main__":
     args = sys.argv[1:]
-    main(filename=first(args) or None)
+    file_name, current_time = get_args(args)
+    main(filename=file_name, current_time=current_time)
--- flows/editors-google-spreadsheet-deposit/test.py
+++ flows/editors-google-spreadsheet-deposit/test.py
@@ -1,7 +1,7 @@
 import unittest
 from io import StringIO
 import json
-from .src import editorscsv2json
+from src import editorscsv2json
 
 
 class TestEditorsCsv2Json(unittest.TestCase):
@@ -9,17 +9,17 @@ class TestEditorsCsv2Json(unittest.TestCase):
 
         self.maxDiff = None
         test_text = (
-            "Name,Keywords,Research interests,Website URL,PubMed URL\n"
-            '"name1","keyword1, keyword2, keyword3","body of research, interest text","http://wwww.eexample.com:8080/sds?ruiu=researcher","http://pubmedurl.org/something?arg=value"\n'
-            '"name1","keyword1, keyword2, keyword3","body of research, interest text","http://wwww.eexample.com:8080/sds?ruiu=researcher","http://pubmedurl.org/something?arg=value"\n'
-            '"name2","keyword1, keyword4, keyword5","body of research interest text2","http://wwww.eexample2.com:8080/sds?ruiu=researcher","http://pubmedurl.org/something2?arg=value"\n'
+            "Name,Subject areas,Keywords,Research interests,Website URL,PubMed URL,Relevant PubMed URLs\n"
+            '"name1","subject1, subject4, subject5","keyword1, keyword2, keyword3","body of research, interest text","http://wwww.eexample.com:8080/sds?ruiu=researcher","http://pubmedurl.org/something?arg=value","url1, url2"\n'
+            '"name1","subject1","keyword1, keyword2, keyword3","body of research, interest text","http://wwww.eexample.com:8080/sds?ruiu=researcher","http://pubmedurl.org/something?arg=value","url3, url4"\n'
+            '"name2","subject7","keyword1, keyword4, keyword5","body of research interest text2","http://wwww.eexample2.com:8080/sds?ruiu=researcher","http://pubmedurl.org/something2?arg=value","url5, url6"\n'
         )
-
+        current_time = '2019-10-16 08:24:32 +0100'
         expected_values = list()
 
-        row_1 = '{"name": "name1", "keywords": "keyword1, keyword2, keyword3", "research_interests": "body of research, interest text", "website_url": "http:\/\/wwww.eexample.com:8080\/sds?ruiu=researcher", "pubmed_url": "http:\/\/pubmedurl.org\/something?arg=value", "editor_role": "BRE"}'
-        row_2 = '{"name": "name1", "keywords": "keyword1, keyword2, keyword3", "research_interests": "body of research, interest text", "website_url": "http:\/\/wwww.eexample.com:8080\/sds?ruiu=researcher", "pubmed_url": "http:\/\/pubmedurl.org\/something?arg=value","editor_role": "BRE"}'
-        row_3 = '{"name": "name2", "keywords": "keyword1, keyword4, keyword5", "research_interests": "body of research interest text2", "website_url": "http:\/\/wwww.eexample2.com:8080\/sds?ruiu=researcher", "pubmed_url": "http:\/\/pubmedurl.org\/something2?arg=value","editor_role": "BRE"}'
+        row_1 = '{"name": "name1","subject_areas":"subject1, subject4, subject5", "keywords": "keyword1, keyword2, keyword3", "research_interests": "body of research, interest text", "website_url": "http:\/\/wwww.eexample.com:8080\/sds?ruiu=researcher", "pubmed_url": "http:\/\/pubmedurl.org\/something?arg=value","relevant_pubmed_urls":"url1, url2", "editor_role": "BRE", "imported_timestamp":"2019-10-16T07:24:32Z"}'
+        row_2 = '{"name": "name1", "subject_areas":"subject1", "keywords": "keyword1, keyword2, keyword3", "research_interests": "body of research, interest text", "website_url": "http:\/\/wwww.eexample.com:8080\/sds?ruiu=researcher", "pubmed_url": "http:\/\/pubmedurl.org\/something?arg=value","relevant_pubmed_urls":"url3, url4","editor_role": "BRE", "imported_timestamp":"2019-10-16T07:24:32Z"}'
+        row_3 = '{"name": "name2","subject_areas":"subject7", "keywords": "keyword1, keyword4, keyword5", "research_interests": "body of research interest text2", "website_url": "http:\/\/wwww.eexample2.com:8080\/sds?ruiu=researcher", "pubmed_url": "http:\/\/pubmedurl.org\/something2?arg=value","relevant_pubmed_urls":"url5, url6","editor_role": "BRE", "imported_timestamp":"2019-10-16T07:24:32Z"}'
         expected_values.append(json.loads(row_1))
         expected_values.append(json.loads(row_2))
         expected_values.append(json.loads(row_3))
@@ -28,13 +28,13 @@ class TestEditorsCsv2Json(unittest.TestCase):
 
         def append_to_list(json_row):
             json_row = json.loads(json_row)
-            json_row.pop("imported_timestamp", None)
             output_list.append(json_row)
 
         editorscsv2json.main(
             input=input_stream,
             output=append_to_list,
-            filename="BREs",
+            filename="filename_BRE",
+            time_now=editorscsv2json.get_utc_time(current_time)
         )
 
         for json_object_index in range(len(output_list)):
@@ -43,6 +43,7 @@ class TestEditorsCsv2Json(unittest.TestCase):
                 expected_values[json_object_index],
                 "Returned and Expected Dictionaries Not Equal",
             )
+
 
 
 if __name__ == "__main__":
