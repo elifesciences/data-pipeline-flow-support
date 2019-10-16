20fb43be6655185c6e382a0e01bbc8f8fffdbc4a
michael <m.owonibi@elifesciences.org>
2019-10-16 12:20:50 +0100

    #5152 import public editor profile based on new schema use same timestamp for all sheets in the spreadsheet imported

--- flows/editors-google-spreadsheet-deposit/src/editorscsv2json.py
+++ flows/editors-google-spreadsheet-deposit/src/editorscsv2json.py
@@ -4,25 +4,27 @@ it must not have any dependencies other than Python3"""
 import sys, json, csv, fileinput, datetime
 from datetime import timezone
 
+
 def normalise(colheader):
     return colheader.lower().strip().replace(" ", "_").replace(")", "").replace("(", "")
 
 
-def get_utc_time(string_datetime:str):
+def get_utc_time(string_datetime: str):
     dtobj = datetime.datetime.strptime(string_datetime, "%Y-%m-%d %H:%M:%S %z")
     dtobj = dtobj.astimezone(tz=timezone.utc)
     return dtobj.strftime("%Y-%m-%dT%H:%M:%SZ")
+
 
 def now():
     dtobj = datetime.datetime.now(timezone.utc)
     return dtobj.strftime("%Y-%m-%d %H:%M:%S %z")
 
 
-def get_args(lst):
+def get_with_default(arg_list: list, index: int, default_val=None):
     try:
-        return lst[0], lst[1]
+        return arg_list[index]
     except IndexError:
-        return None, now()
+        return default_val
 
 
 def empty_fields_are_null(row):
@@ -41,12 +43,12 @@ def main(input=None, output=None, filename=None, time_now=None):
 
     header_reader = csv.reader(fh)
     header = list(map(normalise, next(header_reader)))
-    editor_role = " ".join(filename.split('_')[1:]) if filename else ""
+    editor_role = " ".join(filename.split("_")[1:]) if filename else ""
     reader = csv.DictReader(fh, fieldnames=header)
 
     for row in reader:
         row["editor_role"] = editor_role
-        row["imported_timestamp"] = time_now
+        row["imported_timestamp"] = get_utc_time(time_now)
         empty_fields_are_null(row)
 
         out(json.dumps(row))
@@ -54,5 +56,6 @@ def main(input=None, output=None, filename=None, time_now=None):
 
 if __name__ == "__main__":
     args = sys.argv[1:]
-    file_name, current_time = get_args(args)
+    file_name = get_with_default(args, 0, None)
+    current_time = get_with_default(args, 1, now())
     main(filename=file_name, time_now=current_time)
--- flows/editors-google-spreadsheet-deposit/test.py
+++ flows/editors-google-spreadsheet-deposit/test.py
@@ -14,7 +14,7 @@ class TestEditorsCsv2Json(unittest.TestCase):
             '"name1","subject1","keyword1, keyword2, keyword3","body of research, interest text","http://wwww.eexample.com:8080/sds?ruiu=researcher","http://pubmedurl.org/something?arg=value","url3, url4"\n'
             '"name2","subject7","keyword1, keyword4, keyword5","body of research interest text2","http://wwww.eexample2.com:8080/sds?ruiu=researcher","http://pubmedurl.org/something2?arg=value","url5, url6"\n'
         )
-        current_time = '2019-10-16 08:24:32 +0100'
+        current_time = "2019-10-16 08:24:32 +0100"
         expected_values = list()
 
         row_1 = '{"name": "name1","subject_areas":"subject1, subject4, subject5", "keywords": "keyword1, keyword2, keyword3", "research_interests": "body of research, interest text", "website_url": "http:\/\/wwww.eexample.com:8080\/sds?ruiu=researcher", "pubmed_url": "http:\/\/pubmedurl.org\/something?arg=value","relevant_pubmed_urls":"url1, url2", "editor_role": "BRE", "imported_timestamp":"2019-10-16T07:24:32Z"}'
@@ -34,7 +34,7 @@ class TestEditorsCsv2Json(unittest.TestCase):
             input=input_stream,
             output=append_to_list,
             filename="filename_BRE",
-            time_now=editorscsv2json.get_utc_time(current_time)
+            time_now=current_time,
         )
 
         for json_object_index in range(len(output_list)):
@@ -43,7 +43,6 @@ class TestEditorsCsv2Json(unittest.TestCase):
                 expected_values[json_object_index],
                 "Returned and Expected Dictionaries Not Equal",
             )
-
 
 
 if __name__ == "__main__":
